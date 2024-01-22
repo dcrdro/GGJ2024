c698e731423748c4d8e832f5bb6639fe37932319
Maksym <zip.way.dev@gmail.com>
2024-01-22 23:57:41 +0200

    - Implement EnemySpawnPoint;

---
+++ Assets/Scripts/EnemyLogic/EnemySpawnPoint.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+using UnityEngine;
+
+namespace EnemyLogic
+{
+  public class EnemySpawnPoint : MonoBehaviour
+  {
+    [SerializeField]
+    private float _timeToSpawn;
+
+    #region Fields
+
+    private float _currentTime;
+
+    #endregion
+
+    private void Awake() => 
+      _currentTime = _timeToSpawn;
+
+    private void Update()
+    {
+      _currentTime -= Time.deltaTime;
+      if (_currentTime <= 0)
+      {
+        SpawnEnemy();
+        enabled = false;
+      }
+    }
+
+    private void SpawnEnemy()
+    {
+      Debug.Log($"{gameObject.name}: Spawn enemy");
+
+      GameObject prefab = PrefabsProvider.Instance.EnemyPrefab;
+      Transform container = ContainersProvider.Instance.EnemiesContainer;
+      
+      Instantiate(prefab, transform.position, Quaternion.identity, container);
+    }
+  }
+}
