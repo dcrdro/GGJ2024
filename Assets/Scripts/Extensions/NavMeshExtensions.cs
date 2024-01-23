d1d997043d31841d695862a12f545fa1793fc064
Maksym <zip.way.dev@gmail.com>
2024-01-23 18:20:42 +0200

    - Add NavMeshExtensions for calculating path distance;

---
+++ Assets/Scripts/Extensions/NavMeshExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Extensions
+{
+  public static class NavMeshExtensions
+  {
+    public static float CalculatePathDistance(NavMeshPath path)
+    {
+      float distance = 0;
+
+      for (int cornerIndex = 0; cornerIndex < path.corners.Length - 1; cornerIndex++)
+        distance += Vector3.Distance(path.corners[cornerIndex], path.corners[cornerIndex + 1]);
+
+      return distance;
+    }
+  }
+}
