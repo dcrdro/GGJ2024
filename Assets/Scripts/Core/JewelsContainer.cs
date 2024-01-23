028fe90fc5155cb3161e9121ab9bc9fcb4bc9b2f
Maksym <zip.way.dev@gmail.com>
2024-01-23 14:09:38 +0200

    - Implement Jewels container;

---
+++ Assets/Scripts/Core/JewelsContainer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+  public class JewelsContainer : MonoBehaviour
+  {
+    public static JewelsContainer Instance;
+    
+    [SerializeField]
+    private GameObject[] _jewels;
+    
+    public GameObject this[int index]
+    {
+      get => _jewels[index];
+      set => _jewels[index] = value;
+    }
+
+    private void Awake() => 
+      Instance = this;
+  }
+}
