7ae22fec1a084b48297880e83a26455a74c810cf
Maksym <zip.way.dev@gmail.com>
2024-01-23 16:02:33 +0200

    - Implement EnemySpawnPoints container;

---
+++ Assets/Scripts/Core/EnemySpawnPointsContainer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+  public class EnemySpawnPointsContainer : MonoBehaviour
+  {
+    public static EnemySpawnPointsContainer Instance;
+    
+    [SerializeField]
+    private Transform[] _spawnPoints;
+
+    #region Properties
+
+    public Transform this[int index] => _spawnPoints[index];
+
+    #endregion
+
+    private void Awake() => 
+      Instance = this;
+  }
+}
