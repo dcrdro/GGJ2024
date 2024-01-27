93e1d2cb93cd6d90af88f641d286477f0313636d
Ruslan Smirnov <rsmirnov.dev@gmail.com>
2024-01-27 17:09:47 +0200

    dynamic camera

---
+++ Assets/Scripts/Game/WorldCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WorldCamera: MonoBehaviour
+{
+    public Camera camera;
+    public Transform target;
+    public Vector3 maxDist;
+    public float maxOffset;
+
+
+    private Vector3 offset;
+    private Vector3 initTargetPos;
+
+
+
+    private void Awake()
+    {
+        offset = transform.position;
+    }
+
+    private void Start()
+    {
+        initTargetPos = target.position;
+    }
+
+    private void LateUpdate()
+    {
+        var x = Mathf.InverseLerp(-maxDist.x, maxDist.x, target.position.x - initTargetPos.x);
+        var z = Mathf.InverseLerp(-maxDist.z, maxDist.z, target.position.z - initTargetPos.z);
+
+        var vx = Mathf.Lerp(-maxOffset, maxOffset, x);
+        var vz = Mathf.Lerp(-maxOffset, maxOffset, z);
+
+        camera.transform.position = offset + new Vector3(vx, 0, vz);
+    }
+
+}
